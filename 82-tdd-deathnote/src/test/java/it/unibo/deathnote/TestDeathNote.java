2200f224ba347b3462a9fd8ff31f5aa1058970fa
Chiara <chiara.emina@studio.unibo.it>
2025-11-10 13:14:32 +0100

    implemented the method testZeroAndNegativeRules()

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,31 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Assertions;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.api.DeathNoteImpl;
+
+class TestDeathNote {
+    private DeathNote blackList = new DeathNoteImpl();
+
+    public void testZeroAndNegativeRules(){
+        try {
+            blackList.getRule(0);
+            Assertions.fail("Passing rule number 0 was possible, but should have thrown an exception");
+        } catch (final IllegalArgumentException e) {
+            assertNotNull(e.getMessage()); // Non-null message
+            assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+        }
+
+        try {
+            blackList.getRule(-1);
+            Assertions.fail("Passing a negative rule number was possible, but should have thrown an exception");
+        } catch (final IllegalArgumentException e) {
+            assertNotNull(e.getMessage()); // Non-null message
+            assertFalse(e.getMessage().isBlank()); // Not a blank or empty message
+        }
+    }
 }
