8260a2cbdb66e1ae168b844cd05c0afcf189f15e
Chiara <chiara.emina@studio.unibo.it>
2025-11-15 23:19:39 +0100

    modified the method addView()

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -4,6 +4,8 @@ import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -14,6 +16,7 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
     private DrawNumberView view;
+    private final List<DrawNumberView> views = new ArrayList<>();
 
     /**
      * Builds a new game controller provided a game model.
@@ -27,10 +30,7 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
     @Override
     public void addView(final DrawNumberView view) {
         Objects.requireNonNull(view, "Cannot set a null view");
-        if (this.view != null) {
-            throw new IllegalStateException("The view is already set! Multiple views are not supported");
-        }
-        this.view = view;
+        this.views.add(view);
         view.setController(this);
         view.start();
     }
